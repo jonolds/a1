e9685ccf07e80d06af2699fc59c30d34d79ffddd
Jon Olds <jonolds@gmail.com>
2018-10-03 21:24:56 -0500

    broken: trying to change tuple/integer order

--- friends/Friends.java
+++ friends/Friends.java
@@ -24,39 +24,42 @@ import org.apache.spark.rdd.RDD;
 import org.apache.spark.sql.SparkSession;
 
 //import scala.Option;
-import scala.Serializable;
 import scala.*;
 //import scala.collection.Map;
 //import scala.collection.immutable.List;
 
+
 @SuppressWarnings("unused")
-public class Friends {	
+public class Friends {
 	public static void main(String[] args) throws IOException, InterruptedException {
-		JavaRDD<String> lines = settings().read().textFile("sociNet.txt").javaRDD();
-		JavaRDD<String[]> tokenized = lines.map(new Function<String, String[]>() { public String[] call(String s) { return s.split("\t"); } });
+		JavaRDD<String> lines = settings().read().textFile("sociNetShort.txt").javaRDD();
 		
-		JavaPairRDD<String, String[]> no_frds = tokenized.filter(x->x.length == 1).mapToPair(new PairFunction<String[], String, String[]>() {
-			public Tuple2<String, String[]> call(String[] strArr) {
-				return new Tuple2<>(strArr[0], new String[] {"","","","","","","","","",""});
+		JavaPairRDD<Integer, Integer[]> tokenized = lines.mapToPair(new PairFunction<String, Integer, Integer[]>() { 
+			public Tuple2<Integer, Integer[]> call(String s) {
+				String[] pers_frds_split = s.split("\t");
+				if(pers_frds_split.length > 1)
+					return new Tuple2<>(Integer.parseInt(pers_frds_split[0]), Arrays.stream(pers_frds_split[1].split(",")).map(x->Integer.parseInt(x)).toArray(Integer[]::new));
+				else
+					return new Tuple2<>(Integer.parseInt(pers_frds_split[0]), null);
 			} });
-	//make a pair for each person that has friends with an (person) and [](friends).		
-		JavaPairRDD<String, String[]> has_frds = tokenized.filter(x->x.length > 1).mapToPair(new PairFunction<String[], String, String[]>() {
-			public Tuple2<String, String[]> call(String[] strArr) {
-				return new Tuple2<>(strArr[0], strArr[1].split(","));
-			} });
-	//Deg_1 relations with tuple2<person, friend> as key, 0 as value
-		JavaPairRDD<Tuple2<String, String>,Integer> pers_fr_0 = has_frds.flatMapToPair(new PairFlatMapFunction<Tuple2<String, String[]>, Tuple2<String, String>, Integer>() {
-			public Iterator<Tuple2<Tuple2<String, String>,Integer>> call(Tuple2<String, String[]> t2) {
-				List<Tuple2<Tuple2<String, String>,Integer>> pairs = new ArrayList<>();
-				for(String s: t2._2)
-					pairs.add(new Tuple2<>(new Tuple2<String, String>(t2._1, s), 0));
+		
+		JavaPairRDD<Integer, Integer[]> has_frds = tokenized.filter(x->x._2 != null);
+		JavaPairRDD<Integer, Integer[]> no_frds = tokenized.filter(x->x._2 == null);
+		
+	//Deg_1 relations with tuple2<person, friend> as key, 0 as value for later subtraction
+		JavaPairRDD<Tuple2<Integer, Integer>,Integer> pers_fr_0 = has_frds.flatMapToPair(new PairFlatMapFunction<Tuple2<Integer, Integer[]>, Tuple2<Integer, Integer>, Integer>() {
+			public Iterator<Tuple2<Tuple2<Integer, Integer>,Integer>> call(Tuple2<Integer, Integer[]> t2) {
+				List<Tuple2<Tuple2<Integer, Integer>,Integer>> pairs = new ArrayList<>();
+				for(Integer s: t2._2)
+					pairs.add(new Tuple2<>(new Tuple2<Integer, Integer>(t2._1, s), 0));
 				return pairs.iterator();
 			} });
+		
 	//Deg2 possibles. Uses has_frds to create a deg2_poss_1 entry for each pair of frds with val=1
-		JavaPairRDD<Tuple2<String, String>,Integer> deg2_poss_1 = has_frds.filter(x->x._2 != null).flatMapToPair(new PairFlatMapFunction<Tuple2<String, String[]>, Tuple2<String, String>, Integer>() {
-			public Iterator<Tuple2<Tuple2<String, String>,Integer>>  call(Tuple2<String, String[]> pers) {
-				List<Tuple2<Tuple2<String, String>,Integer>> d2 = new ArrayList<>();
-				for(String friend : pers._2)
+		JavaPairRDD<Tuple2<Integer, Integer>,Integer> deg2_poss_1 = has_frds.filter(x->x._2 != null).flatMapToPair(new PairFlatMapFunction<Tuple2<Integer, Integer[]>, Tuple2<Integer, Integer>, Integer>() {
+			public Iterator<Tuple2<Tuple2<Integer, Integer>,Integer>>  call(Tuple2<Integer, Integer[]> pers) {
+				List<Tuple2<Tuple2<Integer, Integer>,Integer>> d2 = new ArrayList<>();
+				for(Integer friend : pers._2)
 					for(int i = 0; i < pers._2.length; i++)
 						if(pers._2[i] != friend)
 							d2.add(new Tuple2<>(new Tuple2<>(friend, pers._2[i]), 1));
@@ -64,25 +67,60 @@ public class Friends {
 			} });
 		
 	//Sum (reduce) the mutual friend counts
-		JavaPairRDD<Tuple2<String, String>,Integer> deg2_poss_sum = deg2_poss_1.reduceByKey((i1, i2) -> i1 + i2);
+		JavaPairRDD<Tuple2<Integer, Integer>,Integer> deg2_sum = deg2_poss_1.reduceByKey((i1, i2) -> i1 + i2);
 	//For each person, remove the recommendations they're already friends with
-		JavaPairRDD<Tuple2<String, String>,Integer> deg2 = deg2_poss_sum.subtractByKey(pers_fr_0);
+		JavaPairRDD<Tuple2<Integer, Integer>,Integer> deg2 = deg2_sum.subtractByKey(pers_fr_0);
 	//Sort the list by Person and Fr(deg2 candidate)
-		JavaPairRDD<Tuple2<String, String>,Integer> sort_pers_fr = deg2.sortByKey(new CompPersFr());
+		JavaPairRDD<Tuple2<Integer, Integer>,Integer> sort_pers_fr = deg2.sortByKey(new CompPersFr());	
 	//Sort the list again by number of recs per candidate
-		JavaPairRDD<Tuple2<String, Integer>,String> sort_pers_numrecs_fr = sort_pers_fr.mapToPair(new PairFunction<Tuple2<Tuple2<String, String>,Integer>, Tuple2<String, Integer>, String>() {
-			public Tuple2<Tuple2<String, Integer>,String> call(Tuple2<Tuple2<String, String>,Integer> t2t2) {
-				return new Tuple2<>(new Tuple2<>(t2t2._1._1, t2t2._2), t2t2._1._2);
-			}
-		}).sortByKey(new CompByCount());
+		JavaPairRDD<Tuple2<Integer, Integer>,Integer> sorted = sortByCount(sort_pers_fr);
+
+		JavaPairRDD<Integer, Tuple2<Integer,Integer>> keys_split = splitKeys(sorted);
+		
+		
+		JavaPairRDD<Integer,Iterable<Tuple2<Integer,Integer>>> not_a_pair = keys_split.groupByKey();
+
+
 
 		
-		sort_pers_numrecs_fr.saveAsTextFile("output");
+		sorted.saveAsTextFile("output/sorted");
+		keys_split.saveAsTextFile("output/keys_split");
+		not_a_pair.saveAsTextFile("output/not_a_pair");
+//		no_counts.saveAsTextFile("output/no_counts");
+//		topTen.saveAsTextFile("output/topTen");
 //		Object[] rdds = new Object[] {"tokenized: ",tokenized, "no_frds: ",no_frds, "has_frds: ",has_frds, "pers_fr_0: ",pers_fr_0, "deg2_poss_1: ",deg2_poss_1, "deg2_poss_sum: ",deg2_poss_sum, "deg2: ",deg2, "sort_pers_fr ",sort_pers_fr, "sort_pers_numrecs_fr: ",sort_pers_numrecs_fr};
 //		printSave(new Object[] {sort_pers_fr, sort_pers_numrecs_fr, rdds});
-		Thread.sleep(180000);
+		Thread.sleep(60000);
 	}
 	
+	static JavaPairRDD<Integer, Tuple2<Integer,Integer>> splitKeys(JavaPairRDD<Tuple2<Integer, Integer>,Integer> t2_key) {
+		JavaPairRDD<Integer, Tuple2<Integer,Integer>> int_key = t2_key.mapToPair(new PairFunction<Tuple2<Tuple2<Integer, Integer>,Integer>, Integer, Tuple2<Integer, Integer>>() {
+			public Tuple2<Integer, Tuple2<Integer,Integer>> call(Tuple2<Tuple2<Integer, Integer>,Integer> t2) {
+				return new Tuple2<>(t2._1._1, new Tuple2<>(t2._1._2, t2._2));
+			}
+		});
+		return int_key;
+	}
+	
+
+	static JavaPairRDD<Tuple2<Integer, Integer>,Integer> sortByCount(JavaPairRDD<Tuple2<Integer, Integer>,Integer> unsorted) {
+		//Swap key._2 with value
+		JavaPairRDD<Tuple2<Integer, Integer>,Integer> swapped = swapKey2Val(unsorted);
+		//Sort by key
+		JavaPairRDD<Tuple2<Integer, Integer>,Integer> sorted_but_swapped = swapped.sortByKey(new CompByCount());
+		//Unswap
+		JavaPairRDD<Tuple2<Integer, Integer>,Integer> sorted = swapKey2Val(sorted_but_swapped);
+		return sorted;
+	}
+	static JavaPairRDD<Tuple2<Integer, Integer>,Integer> swapKey2Val(JavaPairRDD<Tuple2<Integer, Integer>,Integer> unsorted) {
+		JavaPairRDD<Tuple2<Integer, Integer>,Integer> swapped = unsorted.mapToPair(new PairFunction<Tuple2<Tuple2<Integer, Integer>,Integer>, Tuple2<Integer, Integer>, Integer>() {
+			public Tuple2<Tuple2<Integer, Integer>,Integer> call(Tuple2<Tuple2<Integer, Integer>,Integer> t2t2) {
+				return new Tuple2<>(new Tuple2<>(t2t2._1._1, t2t2._2), t2t2._1._2);
+			}
+		});
+		return swapped;
+	}
+
 	@SuppressWarnings("unchecked")
 	static void printSortPersNumrecsFr(Object objs) throws InterruptedException {
 		System.out.println("\n" +"sort_pers_numrecs_fr: ");
@@ -99,14 +137,14 @@ public class Friends {
 		
 	}
 	
-	static class CompPersFr implements Comparator<Tuple2<String, String>>, Serializable {
-		public int compare(Tuple2<String, String> a, Tuple2<String, String> b) {
-			return (s2int(a._1) > s2int(b._1)) ? 1: (s2int(a._1) < s2int(b._1)) ? -1 : (s2int(a._2) >s2int( b._2)) ? 1 : (s2int(a._2) < s2int(b._2)) ? -1 : 0;
+	static class CompPersFr implements Comparator<Tuple2<Integer, Integer>>, Serializable {
+		public int compare(Tuple2<Integer, Integer> a, Tuple2<Integer, Integer> b) {
+			return (a._1 > b._1) ? 1: (a._1 < b._1) ? -1 : (a._2 > b._2) ? 1 : (a._2 < b._2) ? -1 : 0;
 		}
 	}
-	static class CompByCount implements Comparator<Tuple2<String, Integer>>, Serializable {
-		public int compare(Tuple2<String, Integer> a, Tuple2<String, Integer> b) {
-			return (s2int(a._1) > s2int(b._1)) ? 1 : (s2int(a._1) < s2int(b._1)) ? -1 : (a._2 > b._2) ? -1 : (a._2 < b._2) ? 1 : 0;
+	static class CompByCount implements Comparator<Tuple2<Integer, Integer>>, Serializable {
+		public int compare(Tuple2<Integer, Integer> a, Tuple2<Integer, Integer> b) {
+			return (a._1 > b._1) ? 1 : (a._1 < b._1) ? -1 : (a._2 > b._2) ? -1 : (a._2 < b._2) ? 1 : 0;
 		}
 	}
 	
@@ -124,13 +162,6 @@ public class Friends {
 		List<Tuple2<Tuple2<String, String>, Integer>> aList2 = ((JavaPairRDD<Tuple2<String, String>,Integer>)sorted).collect();
 		for(Tuple2<Tuple2<String, String>, Integer> t2: aList2)
 			System.out.println("	("+ t2._1._1 + "," + t2._1._2 + ") - " + t2._2);
-	}
-
-	
-
-	
-	static Integer s2int(String s) {
-		return Integer.parseInt(s);
 	}
 	
 	static SparkSession settings() throws IOException {
